be34b2101a0f0ead650db4c3ea1757efd2203657
tombeck <t@ocupop.com>
2020-06-10 05:43:12 -0700

    update authnav to use redux for setting currency

--- src/components/auth/AuthNav.jsx
+++ src/components/auth/AuthNav.jsx
@@ -1,24 +1,25 @@
-import React, { useState } from 'react'
-// import PropTypes from 'prop-types'
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 import { ButtonGroup, Button } from 'react-bootstrap'
+import {setCurrency} from '../../shopify/shopifyActions'
 
 const AuthNav = () => {
-  const [currency, setCurrency] = useState("CAD")
-
+  const currency = useSelector(state => state.shopify.currency)
+  const dispatch = useDispatch()
   return (
     <div className="d-flex align-items-center h-100">
       <div className="my-auto mr-5">
         <ButtonGroup size="sm">
           <Button
             variant="dark"
-            onClick={() => setCurrency('USD')}
+            onClick={() => dispatch(setCurrency('USD'))}
             active={currency === 'USD' ? true : false }>
             <span className="flag-icon flag-icon-us mr-2"></span>
             USA
           </Button>
           <Button
             variant="dark"
-            onClick={() => setCurrency('CAD')}
+            onClick={() => dispatch(setCurrency('CAD'))}
             active={currency === 'CAD' ? true : false}>
             <span className="flag-icon flag-icon-ca mr-2"></span>
             Canada
@@ -29,8 +30,6 @@ const AuthNav = () => {
   )
 }
 
-AuthNav.propTypes = {
-
-}
+AuthNav.propTypes = {}
 
 export default AuthNav
