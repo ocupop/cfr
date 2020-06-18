cd3be0c09b6a8e345707423375d03d4385fd285f
tombeck <t@ocupop.com>
2020-06-18 06:59:37 -0700

    fix for reponsive header

--- src/components/cart/CartNav.jsx
+++ src/components/cart/CartNav.jsx
@@ -6,10 +6,10 @@ const CartNav = () => {
   const checkout = useSelector(state => state.shopify.checkout)
 
   return (
-    <Link to='/cart' className="text-secondary">
+    <Link to='/cart' className="text-inherit">
       <div className="d-flex align-items-center">
         <i className="ri-shopping-cart-fill"></i>
-        <span className="ml-2 badge badge-pill badge-secondary">{checkout && checkout.lineItems.length}</span>
+        <span className="ml-2 badge badge-pill badge-white">{checkout && checkout.lineItems.length}</span>
       </div>
     </Link>
   )
