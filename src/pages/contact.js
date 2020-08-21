f55e527fc80ea4b6881bad9fb83034e5873e460b
Jon Burr <jonathanwburr@gmail.com>
2020-08-20 17:49:35 -0700

    related products grid, comment out test cognito form load

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,17 +1,17 @@
 import React, { useEffect }from 'react'
 
 const contactPage = () => {
-  useEffect(() => {
-    const script = document.createElement("script");
-    script.src = "https://www.cognitoforms.com/s/bbN8iw1MJUqjPe6aHn-_rw";
-    script.async = true;
-    script.onload = () => this.scriptLoaded();
+  // useEffect(() => {
+  //   const script = document.createElement("script");
+  //   script.src = "https://www.cognitoforms.com/s/bbN8iw1MJUqjPe6aHn-_rw";
+  //   script.async = true;
+  //   script.onload = () => this.scriptLoaded();
 
-    document.body.appendChild(script)
-    scriptLoaded() {
-      Cognito.load("forms", { id: "1" })
-    }
-  }, [])
+  //   document.body.appendChild(script)
+  //   scriptLoaded() {
+  //     Cognito.load("forms", { id: "1" })
+  //   }
+  // }, [])
   return (
     <div>
       <div className="cognito">
