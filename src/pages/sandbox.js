a2e81843ca361c042e60a743cf757d768aa02f85
tombeck <t@ocupop.com>
2020-08-26 11:49:25 -0700

    sandbox form

--- src/pages/sandbox.js
+++ src/pages/sandbox.js
@@ -4,10 +4,9 @@ import LoadingComponent from '../common/ui/LoadingComponent'
 
 
 const SandboxPage = () => {
-  const checkout = useSelector(state => state.shopify.checkout)
-  const currency = useSelector(state => state.shopify.currency)
+  const activeChannel = useSelector(state => state.shopify.activeChannel)
 
-  if (!checkout || !currency) {
+  if (!activeChannel) {
     return <LoadingComponent />
   }
 
@@ -19,15 +18,35 @@ const SandboxPage = () => {
             <div className="col-12 col-md-8">
               <div className="p-5">
                 <h5>Sandbox</h5>
+                <form name="contact" method="POST" data-netlify="true">
+                  <p>
+                    <label>Your Name: <input type="text" name="name" /></label>
+                  </p>
+                  <p>
+                    <label>Your Email: <input type="email" name="email" /></label>
+                  </p>
+                  <p>
+                    <label>Your Role: <select name="role[]" multiple>
+                      <option value="leader">Leader</option>
+                      <option value="follower">Follower</option>
+                    </select></label>
+                  </p>
+                  <p>
+                    <label>Message: <textarea name="message"></textarea></label>
+                  </p>
+                  <p>
+                    <button type="submit">Send</button>
+                  </p>
+                </form>
+
               </div>
             </div>
             <div className="col-12 col-md-4 bg-dark bg-bleed text-white">
               <div className="px-3 pt-5 text-center">
-                <h5>State Summary:</h5>
+                <h5>Application State Summary:</h5>
                 <hr />
                 <p>
-                  <strong>Currency:</strong> {currency}<br />
-                  <strong>CheckoutId:</strong> {checkout.id}<br />
+                  <strong>Active Sales Channel:</strong> {activeChannel}
                 </p>
               </div>
             </div>
